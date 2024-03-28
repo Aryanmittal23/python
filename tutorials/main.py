7a2f0e6b9a1e7be28b93436e8c71ef36afd3eb5e
Aryanmittal23 <mittalaryan932@gmail.com>
2024-03-28 17:02:29 +0530

    tut

---
+++ tutorials/main.py
@@ -0,0 +1,78 @@
+# name = "Aryan"
+# print(name)
+
+# x =1
+# y=23.0
+# z='3'
+# print(type(x))
+
+# name1 = input("waht is your name?")
+# print('hello '+ name1)
+
+# import math
+
+# pi = 3.14
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi,2))
+# print(math.sqrt(420))
+
+
+# capitals ={"USA":'Washington DC',
+#             'India':'New Delhi',
+#             'China':'Beijing',
+#             'Russia':'Moscow'
+#             }
+
+
+# # capitals.update({'Germany':'Berlin'})
+# # print(capitals['Germany'])
+# # print(capitals.keys())
+# # print(capitals.values())
+# # print(capitals.items())
+
+# capitals.pop('China')
+
+# for key,value in capitals.items():
+#     print(key,value)
+
+# # index operator
+
+# name = "Bro Code"
+
+# if(name[0].islower()):
+#     name=name.cpaitalize()
+
+# # functions
+
+# def hello(name):
+#     print("hello world"+" "+name)
+
+# hello('Aryan')    
+
+# #str.format()= optional method that gives users
+# #              more control when displaying output
+
+animal = 'cow'
+item = 'moon'
+
+print("The {} jumped over {}".format(animal,item))
+
+import random
+
+x = random.randint(1,6)
+y =random.random()
+
+myList = ['rock','paper','scissors']
+z = random.choice(myList)
+
+cards = [1,2,3,4,5,6,7,8,9,'j','Q','K','A']
+
+random.shuffle(cards)
+print(cards)
+
+import shutil
+
+shutil.copyfile
